30e8af604f49030ad2c3ecfa560a979ec205adff
Essaim <337364541@qq.com>
2019-11-03 19:01:15 +0800

    main

--- model/CNN_AE.py
+++ model/CNN_AE.py
@@ -2,3 +2,22 @@ import numpy as np
 from torch import nn
 import torch
 import h5py
+
+
+
+class Model_Pretrain_CNN(nn.Module):
+    def __init__(self, filter_num):
+
+        super(Model_Pretrain_CNN, self).__init__()
+
+        self.encoder = nn.Sequential(nn.Conv2d(1, filter_num[0], (3, 3), stride=1, padding=0),
+                                     nn.Conv2d(filter_num[0], filter_num[1], (3, 3), stride=1, padding=0),
+                                     nn.Conv2d(filter_num[1], 1, (3, 3), stride=1, padding=0))
+        self.decoder = nn.Sequential(nn.ConvTranspose2d(1, filter_num[1], (3, 3), stride=1, padding=0),
+                                     nn.ConvTranspose2d(filter_num[1], filter_num[0], (3, 3), stride=1, padding=0),
+                                     nn.ConvTranspose2d(filter_num[0], 1, (3, 3), stride=1, padding=0))
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
--- train/main.py
+++ train/main.py
@@ -1,32 +1,50 @@
-
-
-
-
 from utils.data_contrainer import get_pretrain_dataloader, get_predict_dataloader
 from utils.load_config import get_config
+from model.CNN_AE import Model_Pretrain_CNN
+from model.LSTM_Prediction import Model_Predict_LSTM
+from train.train import train_pretrain, train_predict
 
 
-def get_model():
-    return
-
-
+def get_model(act, encoder=None, decoder=None, input_size=None, output_size=None):
+    if act == 'pretrian':
+        return Model_Pretrain_CNN(get_config("filters"))
+    elif act == 'train':
+        return Model_Predict_LSTM(encoder, decoder, input_size, output_size)
+    else:
+        print("act error")
 
 
 def main():
     pretrain_datapath = f"data/{get_config('pretrain_data_path')}"
     predict_datapath = f"data/{get_config('predict_data_path')}"
 
+    loss_func, opt = [], []
+    pretrain_dataloader = get_pretrain_dataloader(pretrain_datapath, get_config("batch_size"), channel=0)
 
-    pretrain_dataloader = get_pretrain_dataloader(pretrain_datapath, get_config("batch_size"), channel = 0)
+    pretrain_model = train_pretrain(model=get_model("pretrain"),
+                                    data_loader=pretrain_dataloader,
+                                    loss_func=loss_func,
+                                    optimizer=opt,
+                                    num_epochs=get_config("pretrain_epochs"),
+                                    tensorboard_folder=get_config("xxx"),
+                                    model_folder=get_config("xxx")
+                                    )
+    encoder, decoder = pretrain_model.encoder, pretrain_model.decoder
 
-
-
-    predict_dataloader = get_predict_dataloader()
-
-
-
-
-
+    predict_dataloader = get_predict_dataloader(xxxxxxx)
+    predict_model = get_model("train",
+                              encoder=encoder,
+                              decoder=decoder,
+                              input_size=get_config("width") * get_config("height"),
+                              output_size=get_config("width") * get_config("height"))
+    train_predict(model=predict_model,
+                  data_loader=predict_dataloader,
+                  loss_func=loss_func,
+                  optimizer=opt,
+                  num_epochs=get_config("train_epochs"),
+                  tensorboard_folder=get_config("xxx"),
+                  model_folder=get_config("xxx")
+                  )
 
 
 if __name__ == '__main__':
