d3adad4867480d4da14465d1cf18850d04662d62
Essaim <337364541@qq.com>
2019-11-05 16:41:45 +0800

    normalization

--- train/main.py
+++ train/main.py
@@ -7,7 +7,7 @@ from utils.data_contrainer import get_spatio_dataloader, get_temporal_dataloader
 from utils.load_config import get_config
 from model.CNN_AE import Model_Spatio_CNN
 from model.LSTM_Prediction import Model_Temporal_LSTM
-from train.train_main import train_spatio, train_temporal
+from train.train_main import train_spatio, train_temporal, train_model
 from utils.loss import RMSELoss
 from utils.normalization import preprocess_normalize
 
@@ -28,9 +28,9 @@ def create_loss(loss_type):
         raise ValueError("Unknown loss type.")
 
 
-def create_opt(par, lr, opt_type, extra_par):
+def create_opt(par, lr, opt_type):
     if opt_type == "Adam":
-        return optim.Adam(par, lr, weight_decay=extra_par)
+        return optim.Adam(par, lr)
     elif opt_type == "SGD":
         return optim.SGD(par, lr, momentum=0.9)
     else:
@@ -49,33 +49,34 @@ def main():
     spatio_data_path = f"../data/{get_config('spatio_data_path')}"
     temporal_data_path = f"../data/{get_config('temporal_data_path')}"
 
-    spaio_data_normal, temporal_data_normal, normal_st = preprocess_normalize(spatio_data_path, temporal_data_path,0)
+    spaio_data_normal, temporal_data_normal, normal_st = preprocess_normalize(spatio_data_path, temporal_data_path, 0)
 
     loss_func = create_loss(get_config("loss_type"))
     spatio_dataloader = get_spatio_dataloader(datapath=spatio_data_path, normal=normal_st,
                                               batch_size=get_config("spatio_batch_size"), channel=0,
                                               pre_train_len=get_config("spatio_train_len"))
     spatio_model = get_model("spatio").to(get_config("device"))
-    spatio_opt = create_opt(spatio_model.parameters(), get_config("learning_rate"), get_config("opt_type"),
-                            get_config("weight_decay"))
+    spatio_opt = create_opt(spatio_model.parameters(), get_config("learning_rate"), get_config("opt_type"))
 
-    spatio_model = train_spatio(model=spatio_model,
-                                data_loader=spatio_dataloader,
-                                normal=normal_st,
-                                loss_func=loss_func,
-                                optimizer=spatio_opt,
-                                num_epochs=get_config("spatio_epochs"),
-                                tensorboard_folder=tensorboard_folder,
-                                model_folder=model_folder)
+    spatio_model = train_model(model=spatio_model,
+                               data_loader=spatio_dataloader,
+                               phases=['train', 'validate'],
+                               normal=normal_st,
+                               loss_func=loss_func,
+                               optimizer=spatio_opt,
+                               num_epochs=get_config("spatio_epochs"),
+                               tensorboard_folder=tensorboard_folder,
+                               model_folder_name=f"{model_folder}/ae_model.pkl")
 
-    # torch.cuda.empty_cache()
-    spatio_model.load_state_dict(torch.load(f"{model_folder}/ae_model.pkl")['model_state_dict'])
+
+    # spatio_model.load_state_dict(torch.load(f"{model_folder}/ae_model.pkl")['model_state_dict'])
+
     encoder, decoder = spatio_model.encoder, spatio_model.decoder
 
-
-    # loss_func = create_loss(get_config("loss_type"))
-    temporal_dataloader = get_temporal_dataloader(datapath=temporal_data_path, normal=normal_st,
-                                                  batch_size=get_config("temporal_batch_size"), encoder=encoder,
+    temporal_dataloader = get_temporal_dataloader(datapath=temporal_data_path,
+                                                  normal=normal_st,
+                                                  batch_size=get_config("temporal_batch_size"),
+                                                  encoder=encoder,
                                                   channel=0,
                                                   depend_list=get_config("temporal_depend_list"))
     temporal_model = get_model("temporal",
@@ -83,16 +84,17 @@ def main():
                                decoder,
                                get_config("lstm_width"),
                                get_config("lstm_height")).to(get_config("device"))
-    temporal_opt = create_opt(temporal_model.parameters(), get_config("learning_rate"), get_config("opt_type"),
-                              get_config("weight_decay"))
-    train_temporal(model=temporal_model,
-                   data_loader=temporal_dataloader,
-                   normal=normal_st,
-                   loss_func=loss_func,
-                   optimizer=temporal_opt,
-                   num_epochs=get_config("temporal_epochs"),
-                   tensorboard_folder=tensorboard_folder,
-                   model_folder=model_folder)
+    temporal_opt = create_opt(temporal_model.parameters(), get_config("learning_rate"), get_config("opt_type"))
+    # temporal_opt= optim.Adam(temporal_model.parameters(), get_config("learning_rate"), weight_decay=get_config("weight_decay"))
+    train_model(model=temporal_model,
+                data_loader=temporal_dataloader,
+                phases=['train', 'validate', 'test'],
+                normal=normal_st,
+                loss_func=loss_func,
+                optimizer=temporal_opt,
+                num_epochs=get_config("temporal_epochs"),
+                tensorboard_folder=tensorboard_folder,
+                model_folder_name=f"{model_folder}/best_model.pkl")
 
 
 if __name__ == '__main__':
--- train/train_main.py
+++ train/train_main.py
@@ -11,15 +11,18 @@ from utils.util import save_model
 from utils.load_config import get_config
 
 
-def train_spatio(model,
-                 data_loader,
-                 normal,
-                 loss_func,
-                 optimizer,
-                 num_epochs,
-                 tensorboard_folder: str,
-                 model_folder):
-    phases = ['train', 'validate']
+
+
+def train_model(model,
+                data_loader,
+                phases,
+                normal,
+                loss_func,
+                optimizer,
+                num_epochs,
+                tensorboard_folder: str,
+                model_folder_name):
+
     since = time.clock()
 
     writer = SummaryWriter(tensorboard_folder)
@@ -73,82 +76,8 @@ def train_spatio(model,
                                 phases}, epoch)
     finally:
         time_elapsed = time.clock() - since
-        print(
-            f"cost time: {time_elapsed} seconds          best val loss: {best_rmse}     best epoch: {save_dict['epoch']}")
-        save_model(f"{model_folder}/ae_model.pkl", **save_dict)
-        model.load_state_dict(save_dict['model_state_dict'])
-    return model
-
-
-def train_temporal(model,
-                   data_loader,
-                   normal,
-                   loss_func,
-                   optimizer,
-                   num_epochs,
-                   tensorboard_folder: str,
-                   model_folder: str):
-    phases = ['train', 'validate', 'test']
-    since = time.clock()
-    writer = SummaryWriter(tensorboard_folder)
-
-    loss_func.to(get_config("device"))
-    model.to(get_config("device"))
-
-    save_dict, best_rmse = {'model_state_dict': copy.deepcopy(model.state_dict()), 'epoch': 0}, 999999
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=.5, patience=2, threshold=1e-3, min_lr=1e-6)
-
-    try:
-        for epoch in range(num_epochs):
-
-            running_loss, running_metrics = {phase: 0.0 for phase in phases}, {phase: dict() for phase in phases}
-
-            for phase in phases:
-                if phase == 'train':
-                    model.train()
-                else:
-                    model.eval()
-
-                steps, ground_truth, prediction = 0, list(), list()
-                tqdm_loader = tqdm(data_loader[phase], phase)
-                for x, y in tqdm_loader:
-
-                    # x.to(get_config("device"))
-                    # y.to(get_config("device"))
-
-                    with torch.set_grad_enabled(phase == 'train'):
-                        y_pred = model(x)
-                        loss = loss_func(y, y_pred)
-
-                        if phase == "train":
-                            optimizer.zero_grad()
-                            loss.backward()
-                            optimizer.step()
-                    ground_truth.append(y.cpu().numpy())
-                    prediction.append(y_pred.detach().cpu().numpy())
-
-                    running_loss[phase] += loss * y.size(0)
-                    steps += y.size(0)
-
-                    tqdm_loader.set_description(f"{phase:8} epoch: {epoch:8} loss{running_loss[phase] / steps:3.6},"
-                                                f"true loss{normal.rmse_transform(running_loss[phase] / steps):3.6}")
-
-                    torch.cuda.empty_cache()
-
-                if phase == 'validate' and running_loss[phase] / len(data_loader[phase].dataset) < best_rmse:
-                    best_rmse = running_loss[phase] / len(data_loader[phase].dataset)
-                    save_dict.update(model_state_dict=copy.deepcopy(model.state_dict()),
-                                     epoch=epoch)
-
-            scheduler.step(running_loss['train'])
-
-            writer.add_scalars('Loss',
-                               {f'{phase} loss': running_loss[phase] / len(data_loader[phase].dataset) for phase in
-                                phases}, epoch)
-    finally:
-        time_elapsed = time.clock() - since
-        print(
-            f"cost time: {time_elapsed} seconds          best val loss: {best_rmse}     best epoch: {save_dict['epoch']}")
-        save_model(f"{model_folder}/best_model.pkl", **save_dict)
+        print(f"cost time: {time_elapsed} seconds   best val loss: {best_rmse}     best epoch: {save_dict['epoch']}")
+        save_model(f"{model_folder_name}", **save_dict)
+        model.load_state_dict(torch.load(f"{model_folder_name}")['model_state_dict'])
         model.load_state_dict(save_dict['model_state_dict'])
     return model
